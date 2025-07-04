4703bdb63a612c8f24eecf128254fcdade92ac26
Sundaram Pandey <thisnewcomputerismine2020@gmail.com>
2025-07-04 15:08:04 +0530

    feat: add cookie parser

--- stayfinder-backend-bun/src/index.ts
+++ stayfinder-backend-bun/src/index.ts
@@ -5,11 +5,13 @@ import authRoutes from './routes/auth.routes';
 import listingRoutes from './routes/listings.routes';
 import bookingRoutes from './routes/bookings.routes'
 import wishlistRoutes from './routes/wishlist.routes';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
